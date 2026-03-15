cd40ec098c97de9ed8c24da33db9d8ee44a15e43
Kris <krisbachan123@hotmail.com>
2026-03-14 21:18:08 -0400

    update styles of header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,13 @@ import './Header.css';
 
 function Header() {
     return (
-        <header className='space-grotesk-bold'>
+        <header className='space-grotesk space-grotesk-bold'>
             <img src="/images/pattern-rings.svg" alt="ring" />
             <h1>Nice to meet you!<br></br> I'm <span className='underline'>Kris Bachan</span>.</h1>
 
-            <p>Based in Canada, I’m a front-end developer passionate about building accessible web apps that users love.</p>
+            <p className='space-grotesk space-grotesk-medium'>Based in Canada, I’m a front-end developer passionate about building accessible web apps that users love.</p>
 
-            <a href="https://www.linkedin.com/in/k2pbac/" className='underline' target="_blank" rel="noopener noreferrer">Contact Me</a>
+            <a href="https://www.linkedin.com/in/k2pbac/" className='underline space-grotesk space-grotesk-bold' target="_blank" rel="noopener noreferrer">CONTACT ME</a>
         </header>
     );
 }
