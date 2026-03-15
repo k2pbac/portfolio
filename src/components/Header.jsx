04fbeecffe1060a7b82b4e167ed84368d5d5fe1d
Kris <krisbachan123@hotmail.com>
2026-03-14 21:35:23 -0400

    add image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,14 @@ import './Header.css';
 function Header() {
     return (
         <header className='space-grotesk space-grotesk-bold'>
-            <img src="/images/pattern-rings.svg" alt="ring" />
+            <img className='rings' src="/images/pattern-rings.svg" alt="ring" />
             <h1>Nice to meet you!<br></br> I'm <span className='underline'>Kris Bachan</span>.</h1>
 
             <p className='space-grotesk space-grotesk-medium'>Based in Canada, I’m a front-end developer passionate about building accessible web apps that users love.</p>
 
             <a href="https://www.linkedin.com/in/k2pbac/" className='underline space-grotesk space-grotesk-bold' target="_blank" rel="noopener noreferrer">CONTACT ME</a>
+
+            <img className='profile-pic' src="/images/image-profile-desktop.webp" alt="profile" />
         </header>
     );
 }
