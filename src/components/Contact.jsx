c476a77865377237dc059226ed7a1f0b6fadc94a
Kris <krisbachan123@hotmail.com>
2026-03-23 18:58:30 -0400

    update colors of bg and spacing

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,23 @@
+import "./Contact.css";
+
 export default function Contact() {
-    return (
-        <div className="contact">
-            <div className="contact-description">
-            <h2 className="heading-xl nunita-bold">Contact</h2>
-            <p className="text-md nunita-medium">I would love to hear about your project and how I could help. Please fill in the form, and I’ll get back to you as soon as possible.</p>
-            </div>
-            <a href="mailto:kris.bachan@gmail.com" className="underline nunita-bold" target="_blank" rel="noopener noreferrer">SEND ME AN EMAIL</a> 
-        </div>
-    );
+  return (
+    <div className="contact">
+      <div className="contact-description">
+        <h2 className="heading-xl nunita-bold">Contact</h2>
+        <p className="text-md nunita-medium">
+          I would love to hear about your project and how I could help. Please
+          fill in the form, and I’ll get back to you as soon as possible.
+        </p>
+      </div>
+      <a
+        href="mailto:kris.bachan@gmail.com"
+        className="underline nunita-bold"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        SEND ME AN EMAIL
+      </a>
+    </div>
+  );
 }
