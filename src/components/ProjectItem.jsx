fa9967a018d7dcbc23b2f66748a354560134c3e2
Kris <krisbachan123@hotmail.com>
2026-04-13 17:09:42 -0400

    finish

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function toTitleCase(str) {
   return str.replace(
     /\w\S*/g,
@@ -12,9 +14,46 @@ export default function ProjectItem({
   imageSrc,
   imageAlt,
 }) {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isButtonHovered, setIsButtonHovered] = useState(false);
+  const [isCodeButtonHovered, setIsCodeButtonHovered] = useState(false);
+
+  const viewProject = !isHovered ? (
+    <div className="view-project-overlay">
+      <p
+        onMouseEnter={() => setIsButtonHovered(true)}
+        onMouseLeave={() => setIsButtonHovered(false)}
+        className={`button-text ${isButtonHovered ? "hovered" : ""}`}
+      >
+        {"View Project".toUpperCase()}
+      </p>
+    </div>
+  ) : null;
+
+  const viewCode = !isHovered ? (
+    <div className="view-project-overlay">
+      <p
+        onMouseEnter={() => setIsCodeButtonHovered(true)}
+        onMouseLeave={() => setIsCodeButtonHovered(false)}
+        className={`button-text-code ${isCodeButtonHovered ? "hovered" : ""}`}
+      >
+        {"View Code".toUpperCase()}
+      </p>
+    </div>
+  ) : null;
+
   return (
     <div className="project-item">
-      <a href={link} target="_blank" rel="noopener noreferrer">
+      {viewProject}
+      {viewCode}
+      <a
+        className="image-container"
+        href={link}
+        target="_blank"
+        rel="noopener noreferrer"
+        onMouseMove={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
         <img src={imageSrc} alt={imageAlt} />
       </a>
       <p className="text-md">{toTitleCase(title)}</p>
